65aa9a565eebb474d74d1998c76288df36cd9be4
Ronsdevelop <rony_aguilera@hotmail.com>
2020-11-01 19:25:12 -0500

    trabajando en proveedores

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,41 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            /*
+             $table->integer('cargo_id')->autoIncrement();
+            $table->string('cargo',45);
+            $table->string('descripcion',100)->nullable();
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+            */
+
+            /*
+             $table->integer('proveedor_id')->autoIncrement();
+            $table->string('rason',100);
+            $table->char('ruc',11);
+            $table->string('direccion',100);
+            $table->string('contacto',100);
+            $table->string('email',100)->nullable();
+            $table->char('nCelula',9);
+            $table->char('nFono',9);
+            $table->text('referencia');
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+
+            /*
+            $table->Integer('cargo_id');
+            $table->foreign('cargo_id')->references('cargo_id')->on('positions');
+            */
+
+
+
+
+
         });
     }
 
