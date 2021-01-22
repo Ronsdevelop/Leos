c6daad48720028c987c7fceae47e9dacc48f19a8
Ronsdevelop <rony_aguilera@hotmail.com>
2021-01-21 20:47:22 -0500

    cambio

--- database/migrations/2021_01_21_091040_create_precio_ventas_table.php
+++ database/migrations/2021_01_21_091040_create_precio_ventas_table.php
@@ -14,12 +14,13 @@ class CreatePrecioVentasTable extends Migration
     public function up()
     {
         Schema::create('precio_ventas', function (Blueprint $table) {
+            $table->unsignedInteger('tipclient_id');
+            $table->foreign('tipclient_id')->references('id')->on('tipo_clientes');
             $table->unsignedInteger('producto_id');
             $table->foreign('producto_id')->references('id')->on('productos');
             $table->unsignedInteger('preciop_id');
             $table->foreign('preciop_id')->references('id')->on('pan_x_sols');
-            $table->unsignedInteger('tipclient_id');
-            $table->foreign('tipclient_id')->references('id')->on('tipo_clientes');
+
             $table->engine = 'InnoDB';
             $table->charset = 'utf8';
             $table->collation = 'utf8_general_ci';
