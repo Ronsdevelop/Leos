14daa4febe56b0cc213fa0747a58cc56f7774246
Ronsdevelop <rony_aguilera@hotmail.com>
2021-02-18 11:04:11 -0500

    18-02-21 - mañana

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,96 +1,134 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Panaderia Leos</title>
+        <link rel="stylesheet" href="{{ asset('welcome/css/style.css') }} ">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
+        {{--   @if (Route::has('login'))
+
+                @auth
+                    <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
+                @endauth
+
+        @endif --}}
+
+        <header>
+            <a href="#" class="logo">Panadería Leos<span>.</span></a>
+            <ul class="navigation">
+                <li><a href="#banner">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="#expert">Expert</a></li>
+                <li><a href="#testimonials">Testimonials</a></li>
+                <li><a href="#contact">Contact</a></li>
+                @if (Route::has('login'))
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <li><a href="{{ url('/home') }}">Home</a></li>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <li><a href="{{ route('login') }}">Login</a></li>
                     @endauth
-                </div>
-            @endif
 
+                @endif
+
+            </ul>
+        </header>
+        <section class="banner" id="banner">
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <h2>Always choose Good</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero nesciunt veritatis numquam doloribus sed illo molestias quam earum mollitia. Commodi modi quae amet alias numquam aliquam minima optio! Nihil, expedita!</p>
+                <a href="#" class="btn">Our Menu</a>
+            </div>
+        </section>
+        <section class="about" id="about">
+            <div class="row">
+                <div class="col50">
+                    <h2 class="titleTex"><span>A</span>bout Us</h2>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore nesciunt perspiciatis, vel est repudiandae reprehenderit exercitationem deserunt velit? Nostrum ea beatae atque blanditiis iure facere quod rerum unde quisquam exercitationem!. <br><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, odio? Odio nam ex beatae illo error possimus voluptate maxime quae animi distinctio deserunt debitis nulla nihil dignissimos, magni, adipisci sed.
+                    </p>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="col50">
+                    <div class="imgBx">
+                        <img src="{{asset('welcome/img/img1.jpg')}}" alt="">
+                    </div>
                 </div>
             </div>
-        </div>
+
+        </section>
+
+        <section class="menu" id="menu">
+            <div class="title">
+                <h2 class="titleTex">Our <span>M</span>enu</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="content">
+                <div class="box">
+                    <div class="imgBx">
+                        <img src="{{asset('welcome/img/menu1.jpg')}}" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>Special Salads </h3>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="imgBx">
+                        <img src="{{asset('welcome/img/menu2.jpg')}}" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>Special Soup</h3>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="imgBx">
+                        <img src="{{asset('welcome/img/menu3.jpg')}}" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>Special Pasta</h3>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="imgBx">
+                        <img src="{{asset('welcome/img/menu4.jpg')}}" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>Special Salad</h3>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="imgBx">
+                        <img src="{{asset('welcome/img/menu5.jpg')}}" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>Special Soup</h3>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="imgBx">
+                        <img src="{{asset('welcome/img/menu6.jpg')}}" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>Special Pasta</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="title">
+               <a href="#" class="btn">view all</a>
+            </div>
+
+        </section>
+
+        <script type="text/javascript">
+            window.addEventListener('scroll',function(){
+                const header = document.querySelector('header');
+                header.classList.toggle("sticky", window.scrollY > 0);
+            });
+        </script>
     </body>
 </html>
