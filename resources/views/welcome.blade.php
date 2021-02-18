c48c69d2483660ad9280d5f806afbf2012250656
Ronsdevelop <rony_aguilera@hotmail.com>
2021-02-18 14:23:22 -0500

    18-02-21 - tarde

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -123,6 +123,82 @@
             </div>
 
         </section>
+        <section class="expert" id="expert">
+            <div class="title">
+                <h2 class="titleTex">Our Kitchen<span>E</span>xpert</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="content">
+                <div class="box">
+                    <div class="imgBx">
+                        <img src="{{asset('welcome/img/expert1.jpg')}}" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>Someone Famous</h3>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="imgBx">
+                        <img src="{{asset('welcome/img/expert2.jpg')}}" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>Someone Famous</h3>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="imgBx">
+                        <img src="{{asset('welcome/img/expert3.jpg')}}" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>Someone Famous</h3>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="imgBx">
+                        <img src="{{asset('welcome/img/expert4.jpg')}}" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>Someone Famous</h3>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="testimonials" id="testimonials">
+            <div class="title white">
+                <h2 class="titleText">They <span>S</span>aid About Us</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="content">
+                <div class="box">
+                    <div class="imgBx">
+                        <img src="{{asset('welcome/img/testi1.jpg')}}" alt="">
+                    </div>
+                    <div class="text">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi at dignissimos deserunt quae expedita? Voluptatibus, </p>
+                        <h3>Someone Famous</h3>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="imgBx">
+                        <img src="{{asset('welcome/img/testi2.jpg')}}" alt="">
+                    </div>
+                    <div class="text">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi at dignissimos deserunt quae expedita? Voluptatibus, </p>
+                        <h3>Someone Famous</h3>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="imgBx">
+                        <img src="{{asset('welcome/img/testi3.jpg')}}" alt="">
+                    </div>
+                    <div class="text">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi at dignissimos deserunt quae expedita? Voluptatibus, </p>
+                        <h3>Someone Famous</h3>
+                    </div>
+                </div>
+
+            </div>
+        </section>
 
         <script type="text/javascript">
             window.addEventListener('scroll',function(){
